cf3ea006b171f9479fafb9cce1eb300023212c9a
sureshbabu-kanini <127301439+sureshbabu-kanini@users.noreply.github.com>
2023-08-05 00:09:23 +0530

    New push

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -18,7 +18,7 @@ namespace ImageGallery.Controllers
         public GalleryController(ImageGalleryContext context, IOptions<BlobOptions> blobOptions)
         {
             _context = context;
-            _blobOptions = blobOptions.Value; // Initialize the field with the BlobOptions value
+            _blobOptions = blobOptions.Value;
         }
 
         [HttpGet]
@@ -42,7 +42,7 @@ namespace ImageGallery.Controllers
             {
                 if (hotelViewModel.Admin_Id != 1)
                 {
-                    return Forbid(); // Return a forbidden response if Admin_Id is not 1
+                    return Forbid(); 
                 }
 
                 if (hotelViewModel.Image == null || hotelViewModel.Image.Length == 0)
@@ -50,14 +50,13 @@ namespace ImageGallery.Controllers
                     return BadRequest("No image file was provided");
                 }
 
-                // Upload the image to Azure Blob Storage
+
                 string imageUrl = await UploadImageToBlobStorage(hotelViewModel.Image);
 
-                // Save the gallery image details to the database
                 Gallery galleryImages = new Gallery
                 {
                     Admin_Id = hotelViewModel.Admin_Id,
-                    Gallery_Image = imageUrl // Save the image URL in the database
+                    Gallery_Image = imageUrl 
                 };
 
                 _context.Galleries.Add(galleryImages);
@@ -73,30 +72,23 @@ namespace ImageGallery.Controllers
 
         private async Task<string> UploadImageToBlobStorage(IFormFile imageFile)
         {
-            // Generate a unique blob name based on the uploaded image
             string blobName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(imageFile.FileName)}";
 
-            // Retrieve the connection string and container name from BlobOptions
             string connectionString = _blobOptions.ConnectionString;
             string containerName = "images";
 
-            // Create the BlobServiceClient
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 
-            // Create the container if it does not exist
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
             await containerClient.CreateIfNotExistsAsync();
 
-            // Get a reference to the blob
             BlobClient blobClient = containerClient.GetBlobClient(blobName);
 
-            // Upload the image to the blob
             using (var stream = imageFile.OpenReadStream())
             {
                 await blobClient.UploadAsync(stream, true);
             }
 
-            // Return the URL of the uploaded image
             return blobClient.Uri.ToString();
         }
     }
